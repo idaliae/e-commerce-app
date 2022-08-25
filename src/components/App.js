2f69c7ff35914e17a2c871c5e2d08c7b4327099d
Idalia E <idalia.espino12@gmail.com>
2022-08-24 21:58:59 -0500

    Footer, Description, Books files

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Footer from "./Footer";
 
-// function App = () => (
-
-// )
+function App() {
+    return (
+        <div>
+           <header>Add a Nav Bar</header> 
+           <p>Content</p>
+           <Footer/>
+        </div>
+    );
+}
 
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer (props) {
+    return (
+        <div>
+            <ul>
+                <li>About Us</li>
+                <li>Contact Info</li>
+                <li>Careers</li>
+            </ul>
+            <p>Founded 2022</p>
+        </div>
+    );
+}
+
+export default Footer;
